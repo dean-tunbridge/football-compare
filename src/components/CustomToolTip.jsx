a24c156e88cebb063eb8a4ffc77788cde5d46e47
Dean Tunbridge <deantunbridgedev@gmail.com>
2025-08-30 15:57:04 +1200

    begin fixing tooltip

--- src/components/CustomToolTip.jsx
+++ src/components/CustomToolTip.jsx
@@ -1,32 +1,21 @@
-export default function CustomToolTip({ active, payload, label }) {
+export default function CustomToolTip({ active, payload }) {
   if (active && payload && payload.length) {
+    const data = payload[0]?.payload
+
     return (
-      <div className="p-4 bg-slate-900/90 flex flex-col gap-4 rounded-md">
-        <p className="text-medium text-lg">{label}</p>
-
-        <p className="text-sm text-[#FF268C]">
-          Premier League Goals:
-          <span className="ml-2">{payload[0].value}</span>
-        </p>
-        <p className="text-sm text-[#FFD500]">
-          La Liga Goals:
-          <span className="ml-2">{payload[1].value}</span>
-        </p>
-        <p className="text-sm text-[#E2001A]">
-          Bundesliga Goals:
-          <span className="ml-2">{payload[2].value}</span>
-        </p>
-
-        <p className="text-sm text-[#0066B2]">
-          Serie A Goals:
-          <span className="ml-2">{payload[3].value}</span>
-        </p>
-
-        <p className="text-sm text-[#003399]">
-          Ligue 1 Goals:
-          <span className="ml-2">{payload[4].value}</span>
-        </p>
+      <div className="p-4 bg-slate-900/90 flex flex-col gap-2 rounded-md text-white">
+        <p className="text-lg font-semibold">{data?.name}</p>
+        <p>Goals: {data?.goals}</p>
+        <p>Assists: {data?.assists}</p>
+        <p>Progressive Carries: {data?.progressiveCarries}</p>
+        <p>Progressive Passes: {data?.progressivePasses}</p>
+        <p>Goals per 90: {data?.goalsPer90}</p>
+        <p>Assists per 90: {data?.assistsPer90}</p>
+        <p>Goals + Assists per 90: {data?.goalsAndAssistsPer90}</p>
+        <p>Pass Completion %: {data?.passCompletionPercentage}</p>
       </div>
     )
   }
+
+  return null
 }
