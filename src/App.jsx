e77c55e9c9ebb79432dfd23f1d4b6f2b2891adfb
Dean Tunbridge <deantunbridgedev@gmail.com>
2025-08-30 15:18:06 +1200

    import components and begin working on components

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import GridItem from './components/GridItem'
+import AreaChartComponent from './components/AreaChartComponent'
+import BarChartComponent from './components/BarChartComponent'
+import LineChartComponent from './components/LineChartComponent'
+import RadarChartComponent from './components/RadarChartComponent'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex min-h-screen flex-col items-center justify-center px-4 md:px-8 xl:px-10">
+        <div className="grid w-full gap-10 max-w-[1400px]">
+          <h1 className="text-4xl">
+            Using Data to compare the aattack minded midfield transfer targets
+            of Tottenham Hotspur in the Summer 25/26 window{' '}
+          </h1>
+          <GridItem title="Area Chart">
+            <AreaChartComponent />
+          </GridItem>
+          <GridItem title="Bar Chart">
+            <BarChartComponent />
+          </GridItem>
+          <GridItem title="Line Chart">
+            <LineChartComponent />
+          </GridItem>
+          <GridItem title="RadarChart">
+            <RadarChartComponent />
+          </GridItem>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
